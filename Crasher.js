87fc71d52d9893a5c37a0837b86ebfe836f34973
Immortall0 <davaspamm@outlook.co.id>
2025-11-05 15:39:46 +0700

    JSCrasher

---
+++ Crasher.js
@@ -0,0 +1,8 @@
+function spamMassiveText(iterations = 10000, textSize = 1000) {
+    const spamText = 'لُلُصّبُلُلصّبُررً ॣ ॣh ॣ ॣ冗'.repeat(textSize); 
+        console.log(spamText);
+       
+    }
+}
+
+spamMassiveText();
